4b02f7bb4df503fc82ac332c0a0d3c9853eaa05d
juliweinhofen <julian.schoenbohm@online.de>
2017-04-24 16:45:13 +0200

    Paramaterübergabe für die Anmeldung eines Users (Create User in database)

--- PDO.php
+++ PDO.php
@@ -1,13 +1,12 @@
 <?php
-$dsn = "mysql:dbhost=mars.iuk.hdm-stuttgart.de;dbname=u-nw051";
+$dsn = "mysql:dbhost=https://mars.iuk.hdm-stuttgart.de;dbname=u-nw051";
 $dbuser = "nw051";
 $dbpass = "ABesoo9ahf";
 $db = new PDO($dsn,$dbuser,$dbpass);
-$sql = "INSERT INTO Benutzer(user_name)
-VALUES ($_Post[user_name_input]);";
+$sql = "INSERT INTO Benutzer (user_name)
+VALUES ('$_POST[user_name_input]');";
 $query = $db->prepare($sql);
 $query->execute();
-while ($zeile = $query->fetchObject()) {
-    echo $zeile->user_name . "<br/>\n";
-}
+
+echo "Your user with the username " . $_POST['user_name_input'] . " has been created."
 ?>

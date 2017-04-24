4a7489f7441ce0a2573e65469d4cc24ef595878f
juliweinhofen <julian.schoenbohm@online.de>
2017-04-24 12:50:42 +0200

    test upload 2

---
+++ PDO.php
@@ -0,0 +1,13 @@
+<?php
+$dsn = "mysql:dbhost=mars.iuk.hdm-stuttgart.de;dbname=u-nw051";
+$dbuser = "nw051";
+$dbpass = "ABesoo9ahf";
+$db = new PDO($dsn,$dbuser,$dbpass);
+$sql = "INSERT INTO Benutzer(user_name)
+VALUES ($_Post[user_name_input]);";
+$query = $db->prepare($sql);
+$query->execute();
+while ($zeile = $query->fetchObject()) {
+    echo $zeile->user_name . "<br/>\n";
+}
+?>

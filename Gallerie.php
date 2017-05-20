5732a3a6fc987416d80b4387a7bc893bf4a43894
ns093 <natascha.setzer@gmx.de>
2017-05-20 14:10:48 +0200

    php Info

---
+++ Gallerie.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Taschi
+ * Date: 16.05.17
+ * Time: 11:09
+ */
---
+++ phpinfo.php
@@ -0,0 +1,9 @@
+<?php
+phpinfo();
+?>
+/**
+ * Created by PhpStorm.
+ * User: Taschi
+ * Date: 20.05.17
+ * Time: 14:08
+ */
